0b207ae998c2cb2a0bae985cbce7ef85bd4630a5
sanjaysingh <sanjay.i.kumar.singh@accenture.com>
2024-01-17 15:40:55 +0530

    update app js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
-import {
-  Button,
-  Flex,
-  Heading,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
+
 import { API, Storage } from 'aws-amplify';
 import {
   Button,
