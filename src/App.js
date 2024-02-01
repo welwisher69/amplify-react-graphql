9ff85fa9ebe1628b1d8acfb61b09d77f9d6fda07
sanjaysingh <sanjay.i.kumar.singh@accenture.com>
2024-02-01 12:43:47 +0530

    removing header text

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ const App = ({ signOut }) => {
 
   return (
     <View className="App">
-      <Heading level={2}marginTop={30}>Accenture Notes App</Heading>
+      <Heading level={2}marginTop={30}>Accenture App</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
